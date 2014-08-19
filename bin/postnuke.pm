46f0d7d9a2aca4a6ecbbb8b336a8008a9cb607eb
Marck <marckiii@safe-mail.net>
2014-08-18 22:43:18 -0300

    Create postnuke.pm

---
+++ bin/postnuke.pm
@@ -0,0 +1,286 @@
+package bin::postnuke;
+use Exporter qw(import);
+our @EXPORT_OK = qw(vpn);
+use LWP::UserAgent;
+
+#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#By Marck -+\/+-                                                                      |
+#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+sub vpn {
+#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+                                                
+$NA    = int(rand(12));#                                                      ______           /
+$A[0]  = 'Mozilla/4.0 (compatible; MSIE 5.0; Windows ME) Opera 5.11 [en]';#   \    0\         /
+$A[1]  = 'Mozilla/4.8 [en] (Windows NT 6.0; U)';#                              +-----+ +-----+ 
+$A[2]  = 'Mozilla/4.0 (compatible; MSIE 5.0; SunOS 5.10 sun4u; X11)';#                /\/\/\/
+$A[5]  = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)';#                      /    
+$A[6]  = 'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1)';#                    /     
+$A[7]  = 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6)';#           /      
+$A[8]  = 'Microsoft Internet Explorer/4.0b1 (Windows 95)';#                       /       
+$A[9]  = 'Opera/8.00 (Windows NT 5.1; U';#                                       /        
+$A[10] = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)';#                     /               
+$A[11] = 'Mozilla/4.0 (compatible; MSIE 5.0; AOL 4.0; Windows 95; c_athome)';# /
+#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+$c = LWP::UserAgent->new(agent => $A[$NA]);#                                   \ /\/\/\/\/\/\
+$c->timeout(30);#                                                               +------------+
+$c->env_proxy;#                                                                               \
+$source = $c->get($ARGV[0])->content;#                                                         \
+#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+if($source eq ""){ print "\n[~] Website offline, try again!\n"; exit(); }#                      |
+if($source =~ /400 URL must be absolute/){ print "\n[~] Error 404!\n\n"; exit(); }#             |
+#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+$expN = 0;
+if (length($source) > 0){ }else{ print "\n[~] Error!\n"; exit(); }
+
+if ($source =~ /<meta name="generator" content="PostNuke (.*) - http:\/\/postnuke.com">/){ 
+$v = $1; 
+}else{ 
+print "Version not found! Want to enter manually?[y/n]: ";
+$ynver=<STDIN>; chomp $ynver;
+if ($ynver eq "n" or $ynver eq "N"){ print "\n"; }
+if ($ynver eq "y" or $ynver eq "Y"){
+ print "Version: ";
+ $v=<STDIN>; chomp $v;
+}
+}
+
+ if ($v =~ /0.764/){ $expN = $expN + 1;
+ print "+-[|] PostNuke <= 0.764 - Blind SQL Injection Exploit\n";
+ print "+--[-] http://www.exploit-db.com/exploits/5292/\n";
+ print "+--[-] http://www.1337day.com/exploit/2763\n\n";
+ }
+
+ if ($v =~ /0.763/){ $expN = $expN + 1;
+ print "+-[|] PostNuke <= 0.763 (PNSV lang) Remote Code Execution Exploit\n";
+ print "+--[-] http://www.exploit-db.com/exploits/2707/\n";
+ print "+--[-] http://www.1337day.com/exploit/1107\n\n";
+ }
+
+ if ($v =~ /0.6(.*)/ or $v =~ /0.7(.*)/){ $expN = $expN + 3;
+ print "+-[|] PostNuke 0.6x/0.7x Multiple Cross-Site Scripting Vulnerabilities\n";
+ print "+--[-] http://www.exploit-db.com/exploits/28163/\n|\n";
+ print "+-[|] PostNuke 0.6x/0.7x NS-Languages Module language Parameter XSS\n";
+ print "+--[-] http://www.exploit-db.com/exploits/27254/\n|\n";
+ print "+-[|] PostNuke 0.6x/0.7x NS-Languages Module language Parameter SQL Injection\n";
+ print "+--[-] http://www.exploit-db.com/exploits/27255/\n\n";
+ }
+
+ if ($v =~ /0.76 RC4b/){ $expN = $expN + 2;
+ print "+-[|] PostNuke 0.76 RC4b Comments Module moderate Parameter XSS\n";
+ print "+--[-] http://www.exploit-db.com/exploits/26187/\n|\n";
+ print "+-[|] PostNuke 0.76 RC4b user.php htmltext Parameter XSS\n";
+ print "+--[-] http://www.exploit-db.com/exploits/26188/\n\n";
+ }
+
+ if ($v =~ /0.750/){ $expN = $expN + 2;
+ print "+-[|] PostNuke <= 0.750 readpmsg.php SQL Injection Exploit\n";
+ print "+--[-] http://www.exploit-db.com/exploits/1030/\n|\n";
+ print "+-[|] PostNuke SQL Injection 0.750=>x\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090042\n\n";
+ }
+
+ if ($v =~ /0.75/ or $v =~ /0.76/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.75/0.76 Blocks Module Directory Traversal Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/25665/\n\n";
+ }
+
+ if ($v =~ /0.760 RC3/ or $v =~ /0.760 rc3/ or $v =~ /0.760-RC3/ or $v =~ /0.760-rc3/){ $expN = $expN + 6;
+ print "+-[|] PostNuke Phoenix 0.760 RC3 OP Parameter Remote Cross-Site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/25366/\n|\n";
+ print "+-[|] PostNuke Phoenix 0.760 RC3 Module Parameter Remote Cross-Site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/25367/\n|\n";
+ print "+-[|] PostNuke Phoenix 0.760 RC3 SID Parameter Remote SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/25368/\n|\n";
+ print "+-[|] PostNuke Non Critical SQL Injection and Include 0.760-RC3=>x\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090045\n|\n";
+ print "+-[|] PostNuke XSS and Full path disclosure 0.760RC3=>x\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090044\n|\n";
+ print "+-[|] PostNuke XSS 0.760{RC2,RC3}\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090043\n\n";
+ }
+
+ if ($v =~ /0.76 RC2/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.76 RC2 Multiple Input Validation Vulnerabilities\n";
+ print "+--[-] http://www.exploit-db.com/exploits/27991/\n\n";
+ }
+
+ if ($v =~ /0.7(.*)/){ $expN = $expN + 3;
+ print "+-[|] PostNuke Phoenix 0.7x CATID Parameter Remote SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/25172/\n|\n";
+ print "+-[|] PostNuke 0.7x Install Script Administrator Password Disclosure Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/24307/\n|\n";
+ print "+-[|] PostNuke Phoenix 0.7x SHOW Parameter Remote SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/25173/\n\n";
+ }
+
+ if ($v =~ /0.703/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.703 caselist Arbitrary Module Include Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/21357/\n\n";
+ }
+
+ if ($v =~ /0.72/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.72 Modules.PHP Cross Site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/21873/\n\n";
+ }
+
+ if ($v =~ /0.723/){ $expN = $expN + 2;
+ print "+-[|] PostNuke 0.723 User.PHP UNAME Cross-Site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22767/\n|\n";
+ print "+-[|] PostNuke 0.723 Multiple Cross-Site Scripting Vulnerabilities\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22761/\n\n";
+ }
+
+ if ($v =~ /0.72(.*)/){ $expN = $expN + 2;
+ print "+-[|] PostNuke Phoenix 0.72x Rating System Denial of Service Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22660/\n|\n";
+ print "+-[|] PostNuke 0.72x Members_List Module Path Disclosure\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22439/\n\n";
+ }
+
+ if ($v =~ /0.726/){ $expN = $expN + 1;
+ print "+-[|] PostNuke Phoenix 0.726 openwindow.php hlpfile Parameter XSS\n";
+ print "+--[-] http://www.exploit-db.com/exploits/24037/\n\n";
+ }
+
+ if ($v =~ /0.6/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.6 Unauthenticated User Login Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/21119/\n\n";
+ }
+
+ if ($v =~ /0.762/){ $expN = $expN + 1;
+ print "+-[|] Sql injection in PostNuke [Admin section]\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2006100035\n\n";
+ }
+
+ if ($v =~ /0.761/){ $expN = $expN + 1;
+ print "+-[|] Multiple vulnerabilities in PostNuke <= 0.761\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2006020060\n\n";
+ }
+
+ if ($v =~ /0.760-RC2/ or $v =~ /0.760 RC2/ or $v =~ /0.760-rc2/ or $v =~ /0.760 rc2/){ $expN = $expN + 4;
+ print "+-[|] PostNuke Critical XSS x=>0.760-RC2\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090035\n|\n";
+ print "+-[|] PostNuke SQL Injection 0.760-RC2=>x\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090034\n|\n";
+ print "+-[|] PostNuke Critical SQL Injection 0.760-RC2=>x\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090033\n|\n";
+ print "+-[|] Ctitical Sql Injection PostNuke 0.760-RC2=>x\n";
+ print "+--[-] cxsecurity.com/issue/WLB-2005090053\n\n";
+ }
+
+ if ($v =~ /0.760-RC4b/ or $v =~ /0.760 RC4b/ or $v =~ /0.760 rc4b/ or $v =~ /0.760-rc4b/){ $expN = $expN + 2;
+ print "+-[|] PostNuke 0.760-RC4b=>x Multiple vulnerabilities\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090047\n|\n";
+ print "+-[|] PostNuke XSS 0.760{RC2,RC3}\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2005090043\n\n";
+ }
+
+ if ($source =~ /(.*)modules.php?op=modload&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.764 Module modload SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/12410/\n";
+ print "+--[-] http://www.1337day.com/exploit/12005\n\n";
+ }
+
+ if ($source =~ /(.*)module=FormExpress&(.*)/ or $source =~ /(.*)index.php?module=FormExpress&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke FormExpress Module Blind SQL Injection\n";
+ print "+--[-] http://www.exploit-db.com/exploits/11784/\n";
+ print "+--[-] http://www.1337day.com/exploit/9705\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=pnEncyclopedia&(.*)/ or $source =~ /(.*)module=pnEncyclopedia&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke Module pnEncyclopedia <= 0.2.0 - SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/5541/\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2008050065\n";
+ print "+--[-] http://www.1337day.com/exploit/2954\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=pnFlashGames&(.*)/ or $source =~ /(.*)module=pnFlashGames&(.*)/){ $expN = $expN + 2;
+ print "+-[|] PostNuke Module pnFlashGames <= 2.5 - SQL Injection Vulnerabilities\n";
+ print "+--[-] http://www.exploit-db.com/exploits/5500/\n";
+ print "+--[-] http://www.1337day.com/exploit/2920\n|\n";
+ print "+-[|] PostNuke pnFlashGames Module 1.5 - Remote SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/3813/\n";
+ print "+--[-] http://www.1337day.com/exploit/1803\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=PostSchedule&(.*)/ or $source =~ /(.*)module=PostSchedule&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke Module PostSchedule (eid) SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/5495/\n";
+ print "+--[-] http://www.1337day.com/exploit/2917\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=v4bJournal&(.*)/ or $source =~ /(.*)module=v4bJournal&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke Module v4bJournal - Remote SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/3835/\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2007050028\n";
+ print "+--[-] http://www.1337day.com/exploit/1817\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?name=Downloads&req=viewdownload&(.*)/ or $source =~ /(.*)name=Downloads&req=viewdownload&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.75/0.76 DL-viewdownload.PHP SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/26189/\n\n";
+ }
+
+ if ($source =~ /(.*)modules.php?op=modload&name=books&(.*)/){ $expN = $expN + 1;
+ print "+-[|] Michael Schatz Books 0.54/0.6 PostNuke Module Cross Site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/21903/\n\n";
+ }
+
+ if ($source =~ /(.*)modules.php?op=modload&name=Stats&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.72x Stats Module Path Disclosure Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22438/\n\n";
+ }
+
+ if ($source =~ /(.*)modules.php?op=modload&name=Downloads&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.6/0.7 Downloads Module TTitle Cross-site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22997/\n\n";
+ }
+
+ if ($source =~ /(.*)modules.php?op=modload&name=Web_Links&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.6/0.7 web_links Module TTitle Cross-site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22998/\n\n";
+ }
+
+ if ($source =~ /(.*)modules.php?op=modload&name=Glossary&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.72x Phoenix Glossary Module SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/22651/\n\n";
+ }
+
+ if ($source =~ /(.*)modules.php?0p=modload&name=Reviews&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke 0.72/0.75 Reviews Module Cross-Site Scripting Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/24324/\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=PostWrap&(.*)/ or $source =~ /(.*)module=PostWrap&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke PostWrap Module Remote Exploit\n";
+ print "+--[-] http://www.exploit-db.com/exploits/800/\n";
+ print "+--[-] http://www.1337day.com/exploit/63\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=subjects&(.*)/ or $source =~ /(.*)module=subjects&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke Modules Factory Subjects Module 2.0 - SQL Injection Vulnerability\n";
+ print "+--[-] http://www.exploit-db.com/exploits/24587/\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=pnAddressBook&(.*)/ or $source =~ /(.*)module=pnAddressBook&(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke Module pnAddressbook SQL Injection Vulnerability\n";
+ print "+--[-] http://www.1337day.com/exploit/17402\n\n";
+ }
+
+ if ($source =~ /(.*)article.php?sid=(.*)/){ $expN = $expN + 1;
+ print "+-[|] PostNuke(article.php) Sql-i Vulnerability\n";
+ print "+--[-] http://www.1337day.com/exploit/16621\n\n";
+ }
+
+ if ($source =~ /(.*)index.php?module=Pagesetter&(.*)/ or $source =~ /(.*)module=Pagesetter&(.*)/){ $expN = $expN + 1;
+ print "+-[|] File Disclosure in Pagesetter for PostNuke\n";
+ print "+--[-] http://cxsecurity.com/issue/WLB-2007030034\n\n";
+ }
+
+ if ($expN==0){ print "\n[~] Exploits not found :'( \n"; exit(); }
+print "-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ".$expN."\n-------------------------------------------------------------------------------\n";
+
+exit();
+}
+
+Marck;
