dd3a4f7c73b73692bea73f55f4cc053e49fd262a
Marck <marckiii@safe-mail.net>
2014-08-27 17:09:01 -0300

    Update drupal.pm

--- bin/drupal.pm
+++ bin/drupal.pm
@@ -36,8 +36,8 @@ if ($source =~ /name="Generator" content="Drupal (.*) (.*)/){ $v = $1;
 }else{ 
 print "Version not found! Want to enter manually?[y/n]: ";
 $ynver=<STDIN>; chomp $ynver;
-if ($ynver eq "n" or $ynver eq "N"){ print "\n"; }
-if ($ynver eq "y" or $ynver eq "Y"){
+if (lc($ynver) eq "n"){ print "\n"; }
+if (lc($ynver) eq "y"){
  print "Version: ";
  $v=<STDIN>; chomp $v;
 }
