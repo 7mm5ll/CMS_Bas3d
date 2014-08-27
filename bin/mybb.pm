d0e5aab471a9e378699e7bee5af10c1181c09744
Marck <marckiii@safe-mail.net>
2014-08-27 17:27:17 -0300

    Update mybb.pm

--- bin/mybb.pm
+++ bin/mybb.pm
@@ -34,8 +34,8 @@ if (length($source) > 0){ }else{ print "\n[~] Error!\n"; exit(); }
 
 print "Version not found! Want to enter manually?[y/n]: ";
 $ynver=<STDIN>; chomp $ynver;
-if ($ynver eq "n" or $ynver eq "N"){ print "\n"; }
-if ($ynver eq "y" or $ynver eq "Y"){
+if (lc($ynver) eq "n"){ print "\n"; }
+if (lc($ynver) eq "y"){
  print "Version: ";
  $v=<STDIN>; chomp $v;
 }
