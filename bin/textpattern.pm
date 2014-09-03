c737e1f3b2b9dee6f2000c46850180008b0af6f5
Marck <marckiii@safe-mail.net>
2014-09-03 14:42:51 -0300

    Update textpattern.pm

--- bin/textpattern.pm
+++ bin/textpattern.pm
@@ -1,6 +1,7 @@
 package bin::textpattern;
 use Exporter qw(import);
 our @EXPORT_OK = qw(vtp);
+use Term::ANSIColor qw(:constants);
 use LWP::UserAgent;
 
 #-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -25,53 +26,53 @@ $c = LWP::UserAgent->new(agent => $A[$NA]);#                                   \
 $c->timeout(30);#                                                               +------------+
 $c->env_proxy;#                                                                               \
 $source = $c->get($ARGV[0])->content;#                                                         \
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-if($source eq ""){ print "\n[~] Website offline, try again!\n"; exit(); }#                      |
-if($source =~ /400 URL must be absolute/){ print "\n[~] Error 404!\n\n"; exit(); }#             |
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#-----------------------------------------------------------------------------------------------+
+if($source eq ""){ print "\n", BOLD WHITE,"[", BOLD RED,"~", BOLD WHITE,"] Website offline, try again!\n"; exit(); }#                      |
+if($source =~ /400 URL must be absolute/){ print "\n[", BOLD RED,"~", BOLD WHITE,"] ", BOLD RED,"Error 404!", BOLD WHITE,"\n\n"; exit(); }#             |
+#-----------------------------------------------------------------------------------------------+
 $expN = 0;
-if (length($source) > 0){ }else{ print "\n[~] Error!\n"; exit(); }
+if (length($source) > 0){ }else{ print "\n[", BOLD RED,"~", BOLD WHITE,"] Error!\n"; exit(); }
 
-print "Version not found! Want to enter manually?[y/n]: ";
+print BOLD WHITE,"Version not found! Want to enter manually?[y/n]: ";
 $ynver=<STDIN>; chomp $ynver;
 if (lc($ynver) eq "n"){ print "\n"; }
 if (lc($ynver) eq "y"){
- print "Version: ";
+ print BOLD WHITE,"Version: ";
  $v=<STDIN>; chomp $v;
 }
 
 
  if ($v =~ /4.2.0/){ $expN = $expN + 3;
- print "+-[|] Textpattern CMS 4.2.0 cross site scripting vulnerability\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2010110030\n|\n";
- print "+-[|] Textpattern CMS 4.2.0 remote file inclusion vulnerability\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2010080094\n";
- print "+--[-] http://www.exploit-db.com/exploits/14823/\n";
- print "+--[-] http://www.1337day.com/exploit/13899\n|\n";
- print "+-[|] Textpattern 4.2.0 Cross Site Scripting\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2011040034\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Textpattern CMS 4.2.0 cross site scripting vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010110030\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Textpattern CMS 4.2.0 remote file inclusion vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010080094\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/14823/\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/13899\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Textpattern 4.2.0 Cross Site Scripting\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2011040034\n\n";
  }
  
  if ($v =~ /4.3.0/){ $expN = $expN + 2;
- print "+-[|] Textpattern 4.3.0 Cross Site Request Forgery\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2011050090\n|\n";
- print "+-[|] Textpattern 4.3.0 CMS XSRF Vulnerability (Change Admin's Password)\n";
- print "+--[-] http://www.1337day.com/exploit/16158\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Textpattern 4.3.0 Cross Site Request Forgery\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2011050090\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Textpattern 4.3.0 CMS XSRF Vulnerability (Change Admin's Password)\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/16158\n\n";
  }
  
  if ($v =~ /4.4.1/){ $expN = $expN + 1;
- print "+-[|] Textpattern CMS 4.4.1 Cross Site Scripting\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2012010035\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Textpattern CMS 4.4.1 Cross Site Scripting\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2012010035\n\n";
  } 
  
  if ($v =~ /1.19/){ $expN = $expN + 1;
- print "+-[|] TextPattern <= 1.19 (publish.php) Remote File Inclusion Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/2646/\n";
- print "+--[-] http://www.1337day.com/exploit/1061\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] TextPattern <= 1.19 (publish.php) Remote File Inclusion Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2646/\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/1061\n\n";
  } 
 
- if ($expN==0){ print "\n[~] Exploits not found :'( \n"; exit(); }
-print "-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ".$expN."\n-------------------------------------------------------------------------------\n";
+ if ($expN==0){ print "\n[", BOLD RED,"~", BOLD WHITE,"] Exploits not found :'( \n"; exit(); }
+print BOLD WHITE,"-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ",BOLD MAGENTA $expN."",BOLD WHITE,"\n-------------------------------------------------------------------------------\n";
 
 exit();
 }
