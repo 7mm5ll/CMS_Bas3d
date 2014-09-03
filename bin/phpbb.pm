551a4d5e281001e4fb570b454699948079d80b80
Marck <marckiii@safe-mail.net>
2014-09-03 15:05:45 -0300

    Update phpbb.pm

--- bin/phpbb.pm
+++ bin/phpbb.pm
@@ -1,6 +1,7 @@
 package bin::phpbb;
 use Exporter qw(import);
 our @EXPORT_OK = qw(vbb);
+use Term::ANSIColor qw(:constants);
 use LWP::UserAgent;
 
 #-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -25,136 +26,135 @@ $c = LWP::UserAgent->new(agent => $A[$NA]);#                                   \
 $c->timeout(30);#                                                               +------------+
 $c->env_proxy;#                                                                               \
 $source = $c->get($ARGV[0])->content;#                                                         \
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-if($source eq ""){ print "\n[~] Website offline, try again!\n"; exit(); }#                      |
-if($source =~ /400 URL must be absolute/){ print "\n[~] Error 404!\n\n"; exit(); }#             |
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#-----------------------------------------------------------------------------------------------+
+if($source eq ""){ print "\n", BOLD WHITE,"[", BOLD RED,"~", BOLD WHITE,"] Website offline, try again!\n"; exit(); }#                      |
+if($source =~ /400 URL must be absolute/){ print "\n[", BOLD RED,"~", BOLD WHITE,"] ", BOLD RED,"Error 404!", BOLD WHITE,"\n\n"; exit(); }#             |
+#-----------------------------------------------------------------------------------------------+
 $expN = 0;
-if (length($source) > 0){ }else{ print "\n[~] Error!\n"; exit(); }
+if (length($source) > 0){ }else{ print "\n[", BOLD RED,"~", BOLD WHITE,"] Error!\n"; exit(); }
 
-print "Version not found! Want to enter manually?[y/n]: ";
+print BOLD WHITE,"Version not found! Want to enter manually?[y/n]: ";
 $ynver=<STDIN>; chomp $ynver;
 if (lc($ynver) eq "n"){ print "\n"; }
 if (lc($ynver) eq "y"){
- print "Version: ";
+ print BOLD WHITE,"Version: ";
  $v=<STDIN>; chomp $v;
 }
 
  if ($v =~ /2.0.21/){ $expN = $expN + 2;
- print "+-[|] phpBB <= 2.0.21 (Poison NULL Byte) Remote Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/2348/\n|\n";
- print "+-[|] phpBB 2.0.21 Privmsg.PHP HTML Injection Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/29442/\n\n";
- }
- 
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB <= 2.0.21 (Poison NULL Byte) Remote Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2348/\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB 2.0.21 Privmsg.PHP HTML Injection Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/29442/\n\n";
+ } 
  
  if ($souce =~ /(.*)kb.php?mode=(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB2 Plus 1.53 'kb.php' SQL Injection Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/33772/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB2 Plus 1.53 'kb.php' SQL Injection Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/33772/\n\n";
  }
  
  if ($souce =~ /(.*)mypage.php?id=(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB MyPage Plugin SQL Injection\n";
- print "+--[-] http://www.exploit-db.com/exploits/18212/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB MyPage Plugin SQL Injection\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/18212/\n\n";
  }
  
  if ($souce =~ /(.*)profile.php?mode=register/){ $expN = $expN + 1;
- print "+-[|] PHPBB MOD [2.0.19] Invitation Only (PassCode Bypass vulnerability)\n";
- print "+--[-] http://www.exploit-db.com/exploits/14440/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] PHPBB MOD [2.0.19] Invitation Only (PassCode Bypass vulnerability)\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/14440/\n\n";
  }
  
  if ($souce =~ /(.*)viewtopic.php?topic=(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB viewtopic.php Arbitrary Code Execution\n";
- print "+--[-] http://www.exploit-db.com/exploits/16890/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB viewtopic.php Arbitrary Code Execution\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/16890/\n\n";
  }
  
  if ($souce =~ /(.*)newtopic.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] Claroline 1.8.9 phpbb/newtopic.php URL XSS \n";
- print "+--[-] http://www.exploit-db.com/exploits/32066/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Claroline 1.8.9 phpbb/newtopic.php URL XSS \n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/32066/\n\n";
  }
  
  if ($souce =~ /(.*)viewtopic.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] Claroline 1.8.9 phpbb/viewtopic.php URL XSS \n";
- print "+--[-] http://www.exploit-db.com/exploits/32068/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Claroline 1.8.9 phpbb/viewtopic.php URL XSS \n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/32068/\n\n";
  }
  
  if ($souce =~ /(.*)functions_portal.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB Add-on Fishing Cat Portal Remote File Inclusion Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/5417/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB Add-on Fishing Cat Portal Remote File Inclusion Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/5417/\n\n";
  }
  
  if ($souce =~ /(.*)irc.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB PJIRC Module 0.5 - 'irc.php' Local File Include Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/31535/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB PJIRC Module 0.5 - 'irc.php' Local File Include Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/31535/\n\n";
  }
  
  if ($souce =~ /(.*)kb.php?mode=article&(.*)/){ $expN = $expN + 1;
- print "+-[|] Fully Modded phpBB (kb.php) Remote SQL Injection Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/5243/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Fully Modded phpBB (kb.php) Remote SQL Injection Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/5243/\n\n";
  }
  
  if ($souce =~ /(.*)filebase.php?d=(.*)&(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB Mod FileBase (id) Remote SQL Injection Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/5236/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB Mod FileBase (id) Remote SQL Injection Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/5236/\n\n";
  }
  
  if ($souce =~ /(.*)garage.php?mode=(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB Garage 1.2.0 Beta3 Remote SQL Injection Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/4686/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB Garage 1.2.0 Beta3 Remote SQL Injection Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/4686/\n\n";
  }
  
  if ($souce =~ /(.*)links.php?t=search&(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB Links MOD 1.2.2 - Remote SQL Injection Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/4346/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB Links MOD 1.2.2 - Remote SQL Injection Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/4346/\n\n";
  }
  
  if ($souce =~ /(.*)index.php?name=PNphpBB2&(.*)/){ $expN = $expN + 1;
- print "+-[|] PNphpBB2 <= 1.2 - (index.php c) Remote SQL Injection Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/4026/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] PNphpBB2 <= 1.2 - (index.php c) Remote SQL Injection Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/4026/\n\n";
  }
  
  if ($souce =~ /(.*)forum.php?c=(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB Minerva Mod <= 2.0.21 build 238a SQL Injection Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/3519/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB Minerva Mod <= 2.0.21 build 238a SQL Injection Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/3519/\n\n";
  }
  
  if ($souce =~ /(.*)lat2cyr.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB lat2cyr Mod 1.0.1 (lat2cyr.php) Remote File Include Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/2546/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB lat2cyr Mod 1.0.1 (lat2cyr.php) Remote File Include Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2546/\n\n";
  }
  
  if ($souce =~ /(.*)functions_rpg_events.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB RPG Events 1.0 functions_rpg_events Remote File Include Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/2548/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB RPG Events 1.0 functions_rpg_events Remote File Include Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2548/\n\n";
  }
  
  if ($souce =~ /(.*)phpbb_security.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB Security <= 1.0.1 (php_security.php) Remote File Include Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/2552/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB Security <= 1.0.1 (php_security.php) Remote File Include Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2552/\n\n";
  }
  
  if ($souce =~ /(.*)lat2cyr.php?(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB lat2cyr Mod 1.0.1 (lat2cyr.php) Remote File Include Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/2546/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB lat2cyr Mod 1.0.1 (lat2cyr.php) Remote File Include Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2546/\n\n";
  }
  
  if ($souce =~ /(.*)alltopics.php?mode=(.*)/){ $expN = $expN + 1;
- print "+-[|] phpBB All Topics Mod <= 1.5.0 (start) Remote SQL Injection Exploit\n";
- print "+--[-] http://www.exploit-db.com/exploits/2248/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpBB All Topics Mod <= 1.5.0 (start) Remote SQL Injection Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2248/\n\n";
  }
  
  if ($souce =~ /(.*)auction_room.php?ar=(.*)/ or $souce =~ /(.*)auction_room.php?order=(.*)/){ $expN = $expN + 1;
- print "+-[|] phpbb-auction 1.x auction_room.php ar Parameter SQL Injection\n";
- print "+--[-] http://www.exploit-db.com/exploits/28281/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpbb-auction 1.x auction_room.php ar Parameter SQL Injection\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/28281/\n\n";
  }
  
  if ($souce =~ /(.*)auction_store.php?mode=store&(.*)/){ $expN = $expN + 1;
- print "+-[|] phpbb-auction 1.x auction_store.php u Parameter SQL Injection\n";
- print "+--[-] http://www.exploit-db.com/exploits/28282/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] phpbb-auction 1.x auction_store.php u Parameter SQL Injection\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/28282/\n\n";
  }
  
- if ($expN==0){ print "\n[~] Exploits not found :'( \n"; exit(); }
-print "-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ".$expN."\n-------------------------------------------------------------------------------\n";
+ if ($expN==0){ print "\n[", BOLD RED,"~", BOLD WHITE,"] Exploits not found :'( \n"; exit(); }
+print BOLD WHITE,"-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ",BOLD MAGENTA $expN."",BOLD WHITE,"\n-------------------------------------------------------------------------------\n";
 
 exit();
 }
