bffd0c5af4e441e7a250c8d548888a5deb0c2c42
Marck <marckiii@safe-mail.net>
2014-09-03 14:43:55 -0300

    Update smf.pm

--- bin/smf.pm
+++ bin/smf.pm
@@ -1,6 +1,7 @@
 package bin::smf;
 use Exporter qw(import);
 our @EXPORT_OK = qw(vsmf);
+use Term::ANSIColor qw(:constants);
 use LWP::UserAgent;
 
 #-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -25,169 +26,168 @@ $c = LWP::UserAgent->new(agent => $A[$NA]);#                                   \
 $c->timeout(30);#                                                               +------------+
 $c->env_proxy;#                                                                               \
 $source = $c->get($ARGV[0])->content;#                                                         \
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-if($source eq ""){ print "\n[~] Website offline, try again!\n"; exit(); }#                      |
-if($source =~ /400 URL must be absolute/){ print "\n[~] Error 404!\n\n"; exit(); }#             |
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#-----------------------------------------------------------------------------------------------+
+if($source eq ""){ print "\n", BOLD WHITE,"[", BOLD RED,"~", BOLD WHITE,"] Website offline, try again!\n"; exit(); }#                      |
+if($source =~ /400 URL must be absolute/){ print "\n[", BOLD RED,"~", BOLD WHITE,"] ", BOLD RED,"Error 404!", BOLD WHITE,"\n\n"; exit(); }#             |
+#-----------------------------------------------------------------------------------------------+
 $expN = 0;
-if (length($source) > 0){ }else{ print "\n[~] Error!\n"; exit(); }
+if (length($source) > 0){ }else{ print "\n[", BOLD RED,"~", BOLD WHITE,"] Error!\n"; exit(); }
 
 if ($source =~ /<a href="http:\/\/www.simplemachines.org\/" title="Simple Machines Forum" target="_blank">Powered by SMF (.*)<\/a>/){ 
 $v = $1; 
 }else{ 
-print "Version not found! Want to enter manually?[y/n]: ";
+print BOLD WHITE,"Version not found! Want to enter manually?[y/n]: ";
 $ynver=<STDIN>; chomp $ynver;
 if (lc($ynver) eq "n"){ print "\n"; }
 if (lc($ynver) eq "y"){
- print "Version: ";
+ print BOLD WHITE,"Version: ";
  $v=<STDIN>; chomp $v;
 }
 }
 
  if ($v =~ /1.1.11/){ $expN = $expN + 3;
- print "+-[|] Simple Machines Forums Cross-Site Scripting Vulnerability \n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2010020012\n|\n";
- print "+-[|] Simple Machines Forum 1.1.11 cross site scripting \n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2009120067\n|\n";
- print "+-[|] Simple Machines Forums (SMF 1.1.11) Multiple Search DDOS\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2010020076\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forums Cross-Site Scripting Vulnerability \n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010020012\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum 1.1.11 cross site scripting \n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2009120067\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forums (SMF 1.1.11) Multiple Search DDOS\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010020076\n\n";
  }
 
  if ($v =~ /1.1.5/){ $expN = $expN + 1;
- print "+-[|] Simple Machines Forum <= 1.1.5 Admin Reset Password Exploit (win32)\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2009080123\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum <= 1.1.5 Admin Reset Password Exploit (win32)\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2009080123\n\n";
  }
 
  if ($v =~ /1.0.2/){ $expN = $expN + 1;
- print "+-[|] SMF Mod Member Awards 1.0.2 Blind SQL Injection Exploit\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2009070106\n";
- print "+--[-] http://www.1337day.com/exploit/5436\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF Mod Member Awards 1.0.2 Blind SQL Injection Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2009070106\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/5436\n\n";
  }
 
  if ($v =~ /1.1.4/){ $expN = $expN + 2;
- print "+-[|] Simple Machines Forum <= 1.1.4 Remote SQL Injection Exploit\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2009040221\n|\n";
- print "+-[|] SMF <= 1.1.4 - Audio CAPTCHA Security Bypass Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/31678/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum <= 1.1.4 Remote SQL Injection Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2009040221\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF <= 1.1.4 - Audio CAPTCHA Security Bypass Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/31678/\n\n";
  }
 
  if ($v =~ /1.1.6/){ $expN = $expN + 2;
- print "+-[|] Simple Machines Forum <= 1.1.6 (LFI) Code Execution Exploit\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2009040145\n|\n";
- print "+-[|] Simple Machines Forum (SMF) 1.1.6 Code Execution Exploit\n";
- print "+--[-] http://www.1337day.com/exploit/4070\n";
- print "+--[-] http://www.exploit-db.com/exploits/6993/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum <= 1.1.6 (LFI) Code Execution Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2009040145\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum (SMF) 1.1.6 Code Execution Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/4070\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/6993/\n\n";
  
  }
 
  if ($v =~ /1.1.7/){ $expN = $expN + 1;
- print "+-[|] SMF 1.1.7 Persistent XSS (requires permision to edit censor)\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2009020005\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 1.1.7 Persistent XSS (requires permision to edit censor)\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2009020005\n\n";
  }
 
  if ($v =~ /1.0.7/){ $expN = $expN + 1;
- print "+-[|] SMF 1.0.7 and lower plus 1.1rc2 and lower - IP spoofing vulnerability/IP ban evasion vulnerability [ Disputed / BOGUS ]\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2007020059\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 1.0.7 and lower plus 1.1rc2 and lower - IP spoofing vulnerability/IP ban evasion vulnerability [ Disputed / BOGUS ]\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2007020059\n\n";
  }
 
  if ($v =~ /1.0.6/){ $expN = $expN + 1;
- print "+-[|] Simple Machines Forum - SMF 'X-Forwarded-For' XSS Vulnerability\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2006030042\n|\n";
-
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum - SMF 'X-Forwarded-For' XSS Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2006030042\n|\n";
  }
 
  if ($v =~ /1.1/){ $expN = $expN + 2;
- print "+-[|] Simple Machines Forum 'SMF Shoutbox' Mod Persistent XSS\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2008020053\n|\n";
- print "+-[|] SMF 1.1 Index.PHP HTML Injection Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/29499/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum 'SMF Shoutbox' Mod Persistent XSS\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2008020053\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 1.1 Index.PHP HTML Injection Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/29499/\n\n";
  }
 
  if ($v =~ /2.0/){ $expN = $expN + 2;
- print "+-[|] Simple Machines forum (SMF) 2.0 session hijacking\n";
- print "+--[-] http://www.1337day.com/exploit/16628\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2011080055\n";
- print "+--[-] http://www.exploit-db.com/exploits/17637/\n|\n";
- print "+-[|] SMF 2.0 / 1.1.14 XSRF Filter Bypass\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2011080168\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines forum (SMF) 2.0 session hijacking\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/16628\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2011080055\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/17637/\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0 / 1.1.14 XSRF Filter Bypass\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2011080168\n\n";
  }
 
  if ($v =~ /1.3.1.3/){ $expN = $expN + 1;
- print "+-[|] SMF Forum Mambo Component <= 1.3.1.3 Include Vulnerability\n";
- print "+--[-] http://www.exploit-db.com/exploits/2021/\n";
- print "+--[-] http://www.1337day.com/exploit/583\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF Forum Mambo Component <= 1.3.1.3 Include Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2021/\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/583\n\n";
  }
 
  if ($v =~ /3.1.1/){ $expN = $expN + 1;
- print "+-[|] eFiction <= 3.1.1 (path_to_smf) Remote File Include Vulnerabilities\n";
- print "+--[-] http://www.exploit-db.com/exploits/3361/\n";
- print "+--[-] http://www.1337day.com/exploit/1534\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] eFiction <= 3.1.1 (path_to_smf) Remote File Include Vulnerabilities\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/3361/\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/1534\n\n";
  }
 
  if ($v =~ /1.1.19/ or $v =~ /2.0.6/){ $expN = $expN + 1;
- print "+-[|] SMF 1.1.19, 2.0.6 Multiple Vulns\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2013120190\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 1.1.19, 2.0.6 Multiple Vulns\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2013120190\n\n";
  }
 
  if ($v =~ /2.0.5/){ $expN = $expN + 3;
- print "+-[|] SMF 2.0.5 Remote Shell Upload Vulnerability\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2013100157\n|\n";
- print "+-[|] SMF 2.0.5 Cross Site Request Forgery [ Disputed / BOGUS ]\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2013090165\n|\n";
- print "+-[|] Simple Machines Forum (SMF) <= 2.0.5 multiple vulnerabilities\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2013080129\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0.5 Remote Shell Upload Vulnerability\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2013100157\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0.5 Cross Site Request Forgery [ Disputed / BOGUS ]\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2013090165\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum (SMF) <= 2.0.5 multiple vulnerabilities\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2013080129\n\n";
  }
 
  if ($v =~ /2.0.1/){ $expN = $expN + 1;
- print "+-[|] SMF <= 2.0.1 - SQL Injection, Privilege Escalation\n";
- print "+--[-] http://www.exploit-db.com/exploits/18214/\n";
- print "+--[-] http://www.1337day.com/exploit/17249\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF <= 2.0.1 - SQL Injection, Privilege Escalation\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/18214/\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/17249\n\n";
  }
 
  if ($v =~ /2.0.4/){ $expN = $expN + 3;
- print "+-[|] SMF 2.0.4 Cross Site Scripting\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2013090110\n|\n";
- print "+-[|] SMF 2.0.4 admin payload code execution\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2013040163\n|\n";
- print "+-[|] SMF 2.0.4 Fake php code injection [ Disputed / BOGUS ]\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2013090164\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0.4 Cross Site Scripting\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2013090110\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0.4 admin payload code execution\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2013040163\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0.4 Fake php code injection [ Disputed / BOGUS ]\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2013090164\n\n";
  }
 
  if ($v =~ /1.1.15/){ $expN = $expN + 1;
- print "+-[|] SMF <= 1.1.15 captach bypass\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2011120073\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF <= 1.1.15 captach bypass\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2011120073\n\n";
  }
 
  if ($v =~ /1.1.16/){ $expN = $expN + 1;
- print "+-[|] SMF Portal 1.1.16 (fckeditor) Arbitrary File Upload Vulnerability [ Disputed / BOGUS ]\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2012010174\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF Portal 1.1.16 (fckeditor) Arbitrary File Upload Vulnerability [ Disputed / BOGUS ]\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2012010174\n\n";
  }
 
  if ($v =~ /1.1.8/){ $expN = $expN + 1;
- print "+-[|] Simple Machines Forum 1.1.8 avatar related remote php file\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2010030137\n";
- print "+--[-] http://www.exploit-db.com/exploits/11905/\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum 1.1.8 avatar related remote php file\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010030137\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/11905/\n\n";
  }
 
  if ($v =~ /1.1.14/){ $expN = $expN + 1;
- print "+-[|] SMF 2.0 / 1.1.14 XSRF Filter Bypass\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2011080168\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0 / 1.1.14 XSRF Filter Bypass\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2011080168\n\n";
  }
  
  if ($v =~ /2.0 RC5/ or $v =~ /2.0 rc5/ or $v =~ /2.0-RC5/ or $v =~ /2.0-rc/){ $expN = $expN + 1;
- print "+-[|] SMF 2.0 RC5 Remote Shell Upload Exploit\n";
- print "+--[-] http://www.1337day.com/exploit/15813\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] SMF 2.0 RC5 Remote Shell Upload Exploit\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/15813\n\n";
  }
  
  if ($v =~ /1.1 RC2/ or $v =~ /1.1 rc2/ or $v =~ /1.1-RC2/ or $v =~ /1.1-rc2/){ $expN = $expN + 2;
- print "+-[|] [Xss] IN SMF 1.1 RC2\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2006100134\n|\n";
- print "+-[|] Simple Machines Forum <=1.1RC2 unset() vulnerabilities\n";
- print "+--[-] http://cxsecurity.com/issue/WLB-2006090003\n\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] [Xss] IN SMF 1.1 RC2\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2006100134\n",BOLD GREEN,"|\n";
+ print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Simple Machines Forum <=1.1RC2 unset() vulnerabilities\n";
+ print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2006090003\n\n";
  }
  
- if ($expN==0){ print "\n[~] Exploits not found :'( \n"; exit(); }
-print "-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ".$expN."\n-------------------------------------------------------------------------------\n";
+ if ($expN==0){ print "\n[", BOLD RED,"~", BOLD WHITE,"] Exploits not found :'( \n"; exit(); }
+print BOLD WHITE,"-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ",BOLD MAGENTA $expN."",BOLD WHITE,"\n-------------------------------------------------------------------------------\n";
 
 exit();
 }
