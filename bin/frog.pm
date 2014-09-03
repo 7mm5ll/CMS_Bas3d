ca2ac14f28d6b3b829fcb548dc21a12d5287d4f0
Marck <marckiii@safe-mail.net>
2014-09-03 15:19:01 -0300

    Update frog.pm

--- bin/frog.pm
+++ bin/frog.pm
@@ -1,6 +1,7 @@
 package bin::frog;
 use Exporter qw(import);
 our @EXPORT_OK = qw(vfrog);
+use Term::ANSIColor qw(:constants);
 use LWP::UserAgent;
 
 #-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -25,54 +26,54 @@ $c = LWP::UserAgent->new(agent => $A[$NA]);#                                   \
 $c->timeout(30);#                                                               +------------+
 $c->env_proxy;#                                                                               \
 $source = $c->get($ARGV[0])->content;#                                                         \
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-if($source eq ""){ print "\n[~] Website offline, try again!\n"; exit(); }#                      |
-if($source =~ /400 URL must be absolute/){ print "\n[~] Error 404!\n\n"; exit(); }#             |
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#-----------------------------------------------------------------------------------------------+
+if($source eq ""){ print "\n", BOLD WHITE,"[", BOLD RED,"~", BOLD WHITE,"] Website offline, try again!\n"; exit(); }#                      |
+if($source =~ /400 URL must be absolute/){ print "\n[", BOLD RED,"~", BOLD WHITE,"] ", BOLD RED,"Error 404!", BOLD WHITE,"\n\n"; exit(); }#             |
+#-----------------------------------------------------------------------------------------------+
 $expN = 0;
-if (length($source) > 0){ }else{ print "\n[~] Error!\n"; exit(); }
+if (length($source) > 0){ }else{ print "\n[", BOLD RED,"~", BOLD WHITE,"] Error!\n"; exit(); }
 
-print "Version not found! Want to enter manually?[y/n]: ";
+print BOLD WHITE,"Version not found! Want to enter manually?[y/n]: ";
 $ynver=<STDIN>; chomp $ynver;
 if (lc($ynver) eq "n"){ print "\n"; }
 if (lc($ynver) eq "y"){
- print "Version: ";
+ print BOLD WHITE,"Version: ";
  $v=<STDIN>; chomp $v;
 }
 
   if ($v =~ /0.7/){ $expN = $expN + 1;
-  print "+-[|] Frogss CMS <= 0.7 - Remote SQL Injection Exploit\n";
-  print "+--[-] http://www.exploit-db.com/exploits/3731/\n";
-  print "+--[-] http://www.1337day.com/exploit/1752\n\n";  
+  print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Frogss CMS <= 0.7 - Remote SQL Injection Exploit\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/3731/\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/1752\n\n";  
   }
 
   if ($v =~ /0.9.5/){ $expN = $expN + 5;
-  print "+-[|] Frog CMS 0.9.5 Cross Site Request Forgery / Cross Site Scripting\n";
-  print "+--[-] http://cxsecurity.com/issue/WLB-2010110113\n|\n";
-  print "+-[|] Frog 0.9.5 - CSRF Vulnerability\n";
-  print "+--[-] http://www.exploit-db.com/exploits/10414/\n";
-  print "+--[-] http://www.1337day.com/exploit/10223\n";
-  print "+--[-] http://www.1337day.com/exploit/10204\n";
-  print "+--[-] http://cxsecurity.com/issue/WLB-2010060052\n|\n";  
-  print "+-[|] Frog CMS 0.9.5 - Multiple CSRF Vulnerabilities\n";
-  print "+--[-] http://www.exploit-db.com/exploits/14329/\n";
-  print "+--[-] http://www.1337day.com/exploit/13291\n";
-  print "+--[-] http://cxsecurity.com/issue/WLB-2010070075\n|\n";
-  print "+-[|] frog cms 0.9.5 - Multiple Vulnerabilities\n";
-  print "+--[-] http://www.exploit-db.com/exploits/15615/\n";
-  print "+--[-] http://www.1337day.com/exploit/14966\n|\n";
-  print "+-[|] Frog CMS 0.9.5 - Arbitrary File Upload\n";
-  print "+--[-] http://www.exploit-db.com/exploits/33983/\n\n";
+  print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Frog CMS 0.9.5 Cross Site Request Forgery / Cross Site Scripting\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010110113\n",BOLD GREEN,"|\n";
+  print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Frog 0.9.5 - CSRF Vulnerability\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/10414/\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/10223\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/10204\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010060052\n",BOLD GREEN,"|\n";  
+  print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Frog CMS 0.9.5 - Multiple CSRF Vulnerabilities\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/14329/\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/13291\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://cxsecurity.com/issue/WLB-2010070075\n",BOLD GREEN,"|\n";
+  print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] frog cms 0.9.5 - Multiple Vulnerabilities\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/15615/\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/14966\n",BOLD GREEN,"|\n";
+  print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] Frog CMS 0.9.5 - Arbitrary File Upload\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/33983/\n\n";
   }
 
   if ($v =~ /0.4/){ $expN = $expN + 1;
-  print "+-[|] CMS Frogss <= 0.4 (podpis) Remote SQL Injection Exploit\n";
-  print "+--[-] http://www.1337day.com/exploit/750\n";
-  print "+--[-] http://www.exploit-db.com/exploits/2262/\n\n";  
+  print BOLD WHITE,"+-[",BOLD GREEN,"|",BOLD WHITE,"] CMS Frogss <= 0.4 (podpis) Remote SQL Injection Exploit\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.1337day.com/exploit/750\n";
+  print BOLD WHITE,"+--[",BOLD GREEN,"-",BOLD WHITE,"]",BOLD YELLOW," http://www.exploit-db.com/exploits/2262/\n\n";  
   }
 
- if ($expN==0){ print "\n[~] Exploits not found :'( \n"; exit(); }
-print "-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ".$expN."\n-------------------------------------------------------------------------------\n";
+ if ($expN==0){ print "\n[", BOLD RED,"~", BOLD WHITE,"] Exploits not found :'( \n"; exit(); }
+print "-------------------------------------------------------------------------------\nEnd...\nPossible exploits found: ",BOLD MAGENTA $expN."",BOLD WHITE,"\n-------------------------------------------------------------------------------\n";
 
 exit();
 }
